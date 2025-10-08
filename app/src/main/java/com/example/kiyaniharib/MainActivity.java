1e49ee030c975291dc28f831b22e5915c848bf9d
Muhammad-bilal-503 <mughalbillal0012345@gmail.com>
2025-10-08 15:35:14 +0500

    Third activity

--- app/src/main/java/com/example/kiyaniharib/MainActivity.java
+++ app/src/main/java/com/example/kiyaniharib/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.kiyaniharib;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +24,34 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+
+
+            TextView textView;
+            ImageView imageView;
+            Button btn;
+            textView=findViewById(R.id.txtView);
+            btn=findViewById(R.id.btn);
+            imageView=findViewById(R.id.imageView);
+
+
+            int[] imageRotator=  new int[]{
+                    R.drawable.ball1,
+                    R.drawable.ic_launcher_foreground,
+                    R.drawable.bg_login_gradient,
+                    R.drawable.ic_launcher_foreground
+            };
+
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Random rn=  new Random();
+                    int num=rn.nextInt();
+                    imageView.setImageResource(imageRotator[num]);
+                }
+            });
+
+
             return insets;
         });
     }
